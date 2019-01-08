b3ccb54e7da6a064de7826803f6cf436d70e27ab
Tyler Watson <leggettwatson.tyler@gmail.com>
2019-01-07 16:17:00 -0800

    Set up webpack, package.json, webpack.config, created first four modules (App, MovieItem, MovieList, Search)..render fake movie list data, and working search functionality)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,6 +24,7 @@ class App extends React.Component {
     }
 
     render (){
+        //fix git
         return (
             <div>
                 <SearchBar onFormSubmission={this.onTermSubmit}/>
